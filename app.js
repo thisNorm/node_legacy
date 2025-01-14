d713005e1bbe76b9a52ba60752bd2b9d13434045
theNorm <kisook2557@gmail.com>
2025-01-14 10:55:52 +0900

    chore : Body-parser 설치

--- app.js
+++ app.js
@@ -11,11 +11,17 @@ app.use(express.static(__dirname+'/public'))
 app.get('/', (req, res) => {
     res.render('index');
 })
+
 app.get('/blog', (req, res) => {
     res.render('blog');
 })
+
 app.get('/users', (req, res) => {
     res.render('users');
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
 })
 
 app.post('/api/contact', (req, res) => {
@@ -27,6 +33,7 @@ app.post('/api/contact', (req, res) => {
     const data = `${name} ${phone} ${email} ${memo}`
 
     res.send(data)
+})
   
 app.listen(port, () => {
     console.log(`Node Legacy App listening on port ${port}`)
