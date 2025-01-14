838306b22365975c8a28f7c7d410d98f047db613
theNorm <kisook2557@gmail.com>
2025-01-14 15:13:39 +0900

    chore : MySQL 커넥션 정보 설정 및 dotenv를 통한 민감 정보 은닉

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
 const mysql = require('mysql2')
+require('dotenv').config();
 const app = express()
 const port = 3000;
 
@@ -13,6 +14,16 @@ app.use(express.static(__dirname+'/public'))
 app.use(bodyParser.urlencoded({ extended: false}))
 // parsing JSON
 app.use(bodyParser.json())
+
+// MySQL Connection
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    insecureAuth: true,   
+})
 
 app.get('/', (req, res) => {
     res.render('index');
