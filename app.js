8aa61505033674f6a8ccd1dc339bc05572b2600a
theNorm <kisook2557@gmail.com>
2025-01-14 11:16:31 +0900

    chore : 메인 어플리케이션이 바디파서를 사용하도록 설정

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const ejs = require('ejs')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000;
 
@@ -7,6 +8,10 @@ app.set('view engine', 'ejs')
 app.set('views', './views')
 // static file serving
 app.use(express.static(__dirname+'/public'))
+// parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false}))
+// parsing JSON
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.render('index');
